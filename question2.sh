7d650c842113a5e0743cea2281c9a397cb431482
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2019-11-12 11:54:58 +0530

    Delete question2.sh

--- question2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "log files of directory:" >> /home/piyush/da/f2.txt
-cat /var/log/syslog | grep home | grep piyush >> /home/piyush/da/f2.txt
-echo "log files of all shell script:" >> /home/piyush/da/f2.txt
-cat /var/log/syslog | grep shell >> /home/piyush/da/f2.txt
-exit 0
-
-
