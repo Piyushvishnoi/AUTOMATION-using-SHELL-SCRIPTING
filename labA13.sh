905b4cf755c6dab382c4724ea81cd030ffbe9a35
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2019-11-12 11:41:18 +0530

    Delete labA13.sh

--- labA13.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "enter file to be decrypted" f
-read -p "Do you want to decrypt now? " res
-case $res in
-y|Y|yes|YES) gpg -d $f.gpg ;;
-*) echo "Folder is still encrypted!!!" ;;
-esac
