fbfc634890e13097b369c1006ef8b38041c58e9e
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-14 23:54:50 +0530

    Update labA11.sh

--- scenarios,,,/labA11.sh
+++ scenarios,,,/labA11.sh
@@ -6,6 +6,6 @@ echo "no of lines: "$(wc -l < $file )
 echo "no of characters: "$(wc -c < $file)
 #echo "owner name: "$(stat -x %u $file )
 ls -lT $file
-#nl -nrz -w3 $file
+#nl -nrz -w3 $file //alternate for listing the content 
 cat -n $file
 done
